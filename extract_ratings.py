507563a59bb03705d2c40d81da8f091312102366
Divya <93126745+DivyaKallapu@users.noreply.github.com>
2021-11-08 04:37:15 -0500

    Update extract_ratings.py
    Removed unnecessary loop

--- extract_ratings.py
+++ extract_ratings.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-class GetRating():
+class GetRating:
     def __init__(self, title):
         """
         Constructor of the class
@@ -14,18 +14,17 @@ class GetRating():
     def get_rating(self):
         # Setting up session
         s = requests.session()
-        for i in range(1):
-            url = "https://www.imdb.com/search/title/?title=" + self.title
-            print(url)
-            try:
-                response = s.get(url)
-                # parse the content using beautifulsoup
-                soup = BeautifulSoup(response.content, features="html.parser")
-                # searching all films containers found
-                containers = soup.find_all("div", class_="lister-item-content")
-                # only checking the top search
-                rating = containers[0].find("div", class_="inline-block ratings-imdb-rating")["data-value"]
-                return rating
+        url = "https://www.imdb.com/search/title/?title=" + self.title
+        print(url)
+        try:
+            response = s.get(url)
+            # parse the content using beautifulsoup
+            soup = BeautifulSoup(response.content, features="html.parser")
+            # searching all films containers found
+            containers = soup.find_all("div", class_="lister-item-content")
+            # only checking the top search
+            rating = containers[0].find("div", class_="inline-block ratings-imdb-rating")["data-value"]
+            return rating
 
-            except Exception:
-                print("Try again with valid combination of tile and release year")
+        except Exception:
+            print("Try again with valid combination of tile and release year")
